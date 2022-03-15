67f0abc2f1e9b1dde18ce7b431a08ad2e13c499a
Lazar Jankovic <jlazar997@gmail.com>
2022-03-15 15:13:28 +0100

    create docker images and infra

--- query/index.js
+++ query/index.js
@@ -50,7 +50,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
 	console.log('Listening on 4002');
 	// get stored events
-	const res = await axios.get('http://localhost:4005/events');
+	const res = await axios.get('http://event-bus-srv:4005/events');
 	//iterates stored events events
 	for (let event of res.data) {
 		console.log('processing event', event.type);
